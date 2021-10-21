83e6955c9c02fa5d0a5776fac7174a7b4564969b
Raphael Höser <raphael@hoeser.info>
2021-10-21 10:15:20 +0200

    Improve logging and typing
    Signed-off-by: Raphael Höser <raphael@hoeser.info>

--- src/main.rs
+++ src/main.rs
@@ -3,40 +3,42 @@ mod thermometer;
 use std::io::{self, Write};
 use std::thread::sleep;
 use std::time::Duration;
+use log::{info, debug};
 
 const TRIGGER_ON: f64 = 70.0;
 const TRIGGER_OFF: f64 = 60.0;
 
 const FAN_PIN: u64 = 23;
 
-fn main() {
-    println!("Fan Control starting");
-    println!("Running Config:");
-    println!("Using GPIO {} for CPU cooler.", FAN_PIN);
-    println!("Trigger Fan On temperature is {}°C", TRIGGER_ON);
-    println!("Trigger Fan Off temperature is {}°C", TRIGGER_OFF);
+fn main() -> Result<(), thermometer::ThermometerError>{
+    env_logger::init();
+    info!("Fan Control starting");
+    info!("Running Config:");
+    info!("Using GPIO {} for CPU cooler.", FAN_PIN);
+    info!("Trigger Fan On temperature is {}°C", TRIGGER_ON);
+    info!("Trigger Fan Off temperature is {}°C", TRIGGER_OFF);
 
     #[cfg(target_os = "windows")]
     let mut fan = fan::Fan::new(FAN_PIN).expect("Unable to create Pin");
     #[cfg(target_os = "linux")]
     let fan = fan::Fan::new(FAN_PIN).expect("Unable to create Pin");
 
-    println!("Fan initialized, start to monitor temperature.");
+    debug!("Fan initialized, start to monitor temperature.");
 
     loop {
-        let temp = thermometer::read_temp();
+        let temp = thermometer::read_temp()?;
 
-        println!("Current temperature is {}°C", temp);
+        debug!("Current temperature is {}°C", temp);
 
         if fan.is_on().expect("Unable to read fan pin value") && temp >= TRIGGER_ON {
-            println!(
+            info!(
                 "CPU Temperature is {}°C >= {} -> Turning Fan On",
                 temp, TRIGGER_ON
             );
             fan.turn_on().expect("Unable to turn fan on");
         }
         if fan.is_on().expect("Unable to read fan pin value") && temp <= TRIGGER_OFF {
-            println!(
+            info!(
                 "CPU Temperature is {}°C <= {} -> Turning Fan Off",
                 temp, TRIGGER_OFF
             );
--- src/thermometer.rs
+++ src/thermometer.rs
@@ -1,7 +1,51 @@
 #[cfg(target_os = "linux")]
+use std::fmt;
+#[cfg(target_os = "linux")]
+use std::io;
+#[cfg(target_os = "linux")]
+use std::num::ParseFloatError;
+#[cfg(target_os = "linux")]
 use std::process::Command;
 #[cfg(target_os = "linux")]
 use std::str;
+
+#[derive(Debug)]
+pub enum ThermometerError {
+  #[cfg(target_os = "linux")]
+  ParseFloatError(ParseFloatError),
+  #[cfg(target_os = "linux")]
+  Utf8Error(str::Utf8Error),
+  #[cfg(target_os = "linux")]
+  IOError(io::Error),
+}
+
+#[cfg(target_os = "linux")]
+impl From<ParseFloatError> for ThermometerError {
+  fn from(err: ParseFloatError) -> Self {
+    Self::ParseFloatError(err)
+  }
+}
+
+#[cfg(target_os = "linux")]
+impl From<str::Utf8Error> for ThermometerError {
+  fn from(err: str::Utf8Error) -> Self {
+    Self::Utf8Error(err)
+  }
+}
+
+#[cfg(target_os = "linux")]
+impl From<io::Error> for ThermometerError {
+  fn from(err: io::Error) -> Self {
+    Self::IOError(err)
+  }
+}
+
+#[cfg(target_os = "linux")]
+impl fmt::Display for ThermometerError {
+  fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+    write!(f, "Thermometer Error")
+  }
+}
 
 #[cfg(target_os = "linux")]
 fn get_temp_cmd() -> Command {
@@ -11,18 +55,15 @@ fn get_temp_cmd() -> Command {
 }
 
 #[cfg(target_os = "linux")]
-pub fn read_temp() -> f64 {
-  let result_vec = get_temp_cmd()
-    .output()
-    .expect("Failed to read temperature")
-    .stdout;
-  let result_temp = str::from_utf8(&result_vec).expect("Command invalid");
+pub fn read_temp() -> Result<f64, ThermometerError> {
+  let result_vec = get_temp_cmd().output()?.stdout;
+  let result_temp = str::from_utf8(&result_vec)?;
   let temp_str = result_temp.split('=').collect::<Vec<&str>>()[1];
   let temp_number_str = temp_str.split('\'').collect::<Vec<&str>>()[0];
-  temp_number_str.parse::<f64>().unwrap()
+  Ok(temp_number_str.parse()?)
 }
 
 #[cfg(target_os = "windows")]
-pub fn read_temp() -> f64 {
-  43.0
+pub fn read_temp() -> Result<f64, ThermometerError> {
+  Ok(43.0)
 }
